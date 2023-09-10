1bfd5b5bff25a96a782a7e47210da5f30f1e2084
Nikolay <nikolayshumik@gmail.com>
2023-09-11 01:36:37 +0300

    дизайн почти

--- analogkufar/main/static/main/js/main.js
+++ analogkufar/main/static/main/js/main.js
@@ -12,3 +12,27 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+function redirectToDetails(adId) {
+    var detailsForm = document.createElement('form');
+    detailsForm.action = "/details/";  // Update this with the correct URL path to the details view
+    detailsForm.method = "get";
+    detailsForm.className = "details-form";
+    detailsForm.innerHTML = `
+    {% csrf_token %}
+    <input type="hidden" name="ad_id" value="${adId}">
+    <button class="buttonMod btn-view-details">Посмотреть детали</button>
+`;
+    document.body.appendChild(detailsForm);
+    detailsForm.submit();
+}
+
+
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    if (window.scrollY > 0) {
+        footer.style.display = 'flex';
+    } else {
+        footer.style.display = 'none';
+    }
+});
