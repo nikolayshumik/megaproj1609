c990b5ae17e62ba967d83ff7facf5ee772847bcb
Nikolay <nikolayshumik@gmail.com>
2023-09-16 04:40:24 +0300

    рабочий поиск на любом урле

--- analogkufar/main/views.py
+++ analogkufar/main/views.py
@@ -309,7 +309,7 @@ def edit_profile(request):
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('profile')
     else:
         form = ProfileForm(instance=profile)
 
