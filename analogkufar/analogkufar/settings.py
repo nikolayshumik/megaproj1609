731f6a5e02be8335eebdfbeb9b0f8e1d0b65ccd6
Nikolay <nikolayshumik@gmail.com>
2023-09-16 04:55:18 +0300

    профиль сохраняется на всех урлах

--- analogkufar/analogkufar/settings.py
+++ analogkufar/analogkufar/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'django_celery_results',
+
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'main.context_processors.title_search_form',
+                'main.context_processors.current_user',
             ],
         },
     },
--- analogkufar/main/context_processors.py
+++ analogkufar/main/context_processors.py
@@ -1,5 +1,9 @@
 from .forms import TitleSearchForm
+from django.contrib.auth import get_user
 
+def current_user(request):
+    user = get_user(request)
+    return {'current_user': user}
 def title_search_form(request):
     search_form = TitleSearchForm(request.GET)
     return {'title_search_form': search_form}
